082f9402683737ee4d10efe4fdd938202fbb6f10
Abhishek Pal <1abhishekpal@gmail.com>
2018-06-04 18:27:21 +0530

    Add files via upload

---
+++ unet.py
@@ -0,0 +1,142 @@
+from __future__ import print_function
+
+import os
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+import os
+import cv2
+from keras.layers import Input,Dense,Flatten,Dropout,concatenate,Reshape,Conv2D,MaxPooling2D,UpSampling2D,Conv2DTranspose
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model,Sequential
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adadelta, RMSprop,SGD,Adam
+from keras import regularizers
+from keras import backend as K
+import numpy as np
+import scipy.misc
+import numpy.random as rng
+from PIL import Image, ImageDraw, ImageFont
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+import math
+
+K.set_image_data_format('channels_last')
+
+img_rows = 176
+img_cols = 176
+
+smooth = 1.
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def get_unet():
+
+    inputs = Input((img_rows, img_cols, 3))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3,3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3,3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='sigmoid', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    up6 = concatenate([conv5, conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    up7 = UpSampling2D((2,2))(conv6)
+    up7 = concatenate([up7, conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    up8 = UpSampling2D((2,2))(conv7)
+    up8 = concatenate([up8, conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    up9 = UpSampling2D((2,2))(conv8)
+    up9 = concatenate([up9, conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
+    decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[decoded])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss='mean_squared_error', metrics=['mse'])
+
+    return model
+
+
+def train_and_predict():
+    imgs_un = np.load('./imgs_un_train.npy')
+    imgs_gt = np.load('./imgs_gt_train.npy')
+    imgs_un = imgs_un.astype('float32')
+    imgs_gt = imgs_gt.astype('float32')
+    imgs_gt /= 255.0
+    imgs_un /= 255.0
+    print(imgs_un.shape)
+    model = get_unet()
+    #model_checkpoint = ModelCheckpoint('./undersampled/weights.h5', monitor='val_loss', save_best_only=True)
+    #model.fit(imgs_un, imgs_gt, batch_size=32, nb_epoch=2000, verbose=1, shuffle=True,validation_split=0.2,callbacks=[model_checkpoint])
+
+    #model.load_weights('./undersampled/weights.h5')
+    #imgs_predicted = model.predict(imgs_un, verbose=1)
+
+    img = imgs_un[0,:,:,0]
+    cv2.imwrite("gen.png",img*255.0)
+if __name__ == '__main__':
+    train_and_predict()
